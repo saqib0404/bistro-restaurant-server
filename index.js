326b43d4330a8bf5d1800d53d21e1212e42717ca
saqib0404 <saqibahmad0404@gmail.com>
2025-06-09 18:32:40 +0600

    mongodb setted and post method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 8000
 
@@ -7,6 +9,49 @@ const PORT = process.env.PORT || 8000
 app.use(express.json());
 app.use(cors())
 
+
+
+const uri = `mongodb+srv://${process.env.BISTRO_USERNAME}:${process.env.BISTRO_PASS}@cluster0.tlbypdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // collections
+        const cartCollection = client.db("bistro_restaurant").collection("cartCollection")
+
+        app.get('/', (req, res) => {
+            res.send("Donee")
+        })
+
+        app.post('/carts', async (req, res) => {
+            const item = req.body
+            const result = await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
