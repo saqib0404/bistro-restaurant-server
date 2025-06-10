bf69f6791f96d0544038bf409600221a6a686649
saqib0404 <saqibahmad0404@gmail.com>
2025-06-10 20:01:33 +0600

    delete api implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 8000
 
@@ -45,6 +45,13 @@ async function run() {
         app.post('/carts', async (req, res) => {
             const item = req.body
             const result = await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
