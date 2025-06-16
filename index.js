bfa6c6b489eb1ccbe5a878bec958cc88255ccd8c
saqib0404 <saqibahmad0404@gmail.com>
2025-06-16 12:37:51 +0600

    users api setted

--- index.js
+++ index.js
@@ -57,8 +57,18 @@ async function run() {
         })
 
         //  User Collection
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body
+            const query = { email: user.email }
+            const isUserPresent = await userCollection.findOne(query)
+            if (isUserPresent) {
+                return res.send({ message: "User already present" })
+            }
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
