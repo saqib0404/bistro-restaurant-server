e5d39619a4cb0733bcab1a05dbf9e7766adc0aa6
saqib0404 <saqibahmad0404@gmail.com>
2025-06-19 09:59:36 +0600

    delete user api added

--- index.js
+++ index.js
@@ -73,6 +73,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     } finally {
         // Ensures that the client will close when you finish/error
