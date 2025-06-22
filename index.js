5a38b3bd334f0cf4c9fcb256f7a64bedee55a3a5
saqib0404 <saqibahmad0404@gmail.com>
2025-06-22 18:27:58 +0600

    modifieed

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ const client = new MongoClient(uri, {
 // middlewares
 const verifyToken = (req, res, next) => {
     if (!req.headers.authorization) {
-        res.status(401).send({ message: "forbiden access" })
+        res.status(401).send({ message: "unathorized access" })
     }
     const token = req.headers.authorization.split(' ')[1]
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            res.status(401).send({ message: "forbiden access" })
+            res.status(401).send({ message: "unathorized access" })
         } else {
             req.decoded = decoded
             next()
@@ -58,8 +58,20 @@ async function run() {
             res.send({ token })
         })
 
+        // verify admin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.user.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === "admin"
+            if (!isAdmin) {
+                return res.status(403).send({ message: "forbiden access" })
+            }
+            next()
+        }
+
         //  User Collection
-        app.get("/users", verifyToken, async (req, res) => {
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
@@ -75,8 +87,8 @@ async function run() {
             res.send(result)
         })
 
-        // make admin
-        app.patch('/users/admin/:id', async (req, res) => {
+        //Admin Related
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const updateDocument = {
@@ -88,7 +100,22 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/users/:id', async (req, res) => {
+        app.get("/users/admin/:email", verifyToken, async (req, res) => {
+            const email = req.params.email
+            // console.log(req.decoded.user.email);
+            if (email !== req.decoded.user.email) {
+                return res.status(403).send({ message: "forbiden access" })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === "admin"
+            }
+            res.send(admin)
+        })
+
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query)
@@ -96,20 +123,20 @@ async function run() {
         })
 
         // Cart Collection
-        app.get("/carts", async (req, res) => {
+        app.get("/carts",  async (req, res) => {
             const email = req.query.email
             const query = { user: email }
             const result = await cartCollection.find(query).toArray();
             res.send(result)
         })
 
-        app.post('/carts', async (req, res) => {
+        app.post('/carts',verifyToken, async (req, res) => {
             const item = req.body
             const result = await cartCollection.insertOne(item)
             res.send(result)
         })
 
-        app.delete('/carts/:id', async (req, res) => {
+        app.delete('/carts/:id',verifyToken, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query)
