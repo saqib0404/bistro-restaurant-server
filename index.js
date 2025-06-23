2f62e5325ceb49dff7819ecd7f4709021d37dc07
saqib0404 <saqibahmad0404@gmail.com>
2025-06-23 11:15:20 +0600

    delete menuitem added

--- index.js
+++ index.js
@@ -166,11 +166,10 @@ async function run() {
         app.delete('/menus/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
-            const menuitemQuery = await menuCollection.findOne(query)
-            console.log(menuitemQuery);
-
-            // const result = await menuCollection.deleteOne(query)
-            // res.send(result)
+            const cartQuery = { foodId: id }
+            const menuitemQuery = await cartCollection.deleteMany(cartQuery);
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
         })
 
     } finally {
